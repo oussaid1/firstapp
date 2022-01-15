b3285ce339673c3134a6b55ebf5f54b8d359423a
Abdellatif <oussaid.abdellatif@gmail.com>
2022-01-15 09:36:28 +0100

    0.0.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
-import 'package:devtest/calcul.dart';
+import 'package:devtest/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return const ProviderScope(
-      child: MaterialApp(
-        home: CalculPage(),
-      ),
+    return const MaterialApp(
+      home: HomePage(),
     );
   }
 }
